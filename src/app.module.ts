28d4bbb03aa309735cc6d7772d79f7229e06971e
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2023-04-10 23:06:07 -0300

    Adding module with TypeOrm, schema validator and .env configuration

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CountriesModule } from './countries/countries.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configSchemaValidation } from './config.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`stage.${process.env.STAGE}.env`],
+      validationSchema: configSchemaValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    CountriesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
