13aa063581f162e4713d44d966f71b94f896f613
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2023-04-18 15:42:15 -0300

    Img swegger

--- swegger.config.ts
+++ swegger.config.ts
@@ -2,7 +2,7 @@ import { DocumentBuilder } from "@nestjs/swagger";
 
 export const sweggerConfig = new DocumentBuilder()
     .setTitle("Countries API")
-    .setDescription("API contruida para o desafio da empresa Club-Petro")
+    .setDescription("API contruida para o desafio da empresa ClubPetro")
     .setVersion("1.0")
     .addTag("countries")
     .build();
